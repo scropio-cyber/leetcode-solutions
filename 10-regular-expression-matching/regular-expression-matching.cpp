ac84620ed1c20a841732baa69221f3fa69c99bbb
Nishant Singh <rajputnishantrajput4774@gmail.com>
2026-08-30 13:10:29 +0530

    Time: 414 ms (6.36%) | Memory: 8.1 MB (85.17%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    bool solve(string& s, string& p, int i, int j) {
-
-        // Pattern completely processed
-        if (j == p.size()) {
-            return i == s.size();
-        }
-
-        // Already calculated
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Does current character match?
-        bool firstMatch =
-            (i < s.size() &&
-             (s[i] == p[j] || p[j] == '.'));
-
-        // Next character is '*'
-        if (j + 1 < p.size() && p[j + 1] == '*') {
-
-            // Option 1: use '*' zero times
-            bool skip = solve(s, p, i, j + 2);
-
-            // Option 2: use '*' one or more times
-            bool use =
-                firstMatch &&
-                solve(s, p, i + 1, j);
-
-            return dp[i][j] = skip || use;
-        }
-
-        // Normal character or '.'
-        if (firstMatch) {
-            return dp[i][j] = solve(s, p, i + 1, j + 1);
-        }
-
-        return dp[i][j] = false;
+bool solve(string &s,string &p,int i,int j)
+{
+    int m=s.length();
+    int n=p.length();
+    
+    //base case
+    if(j==n)
+    {
+        if(i==m)
+        return(true);
+        else
+        return(false);
     }
+    bool match=false;
+    if(i<m && ((s[i]==p[j]) || p[j]=='.'))
+    match=true;
 
+    if((j+1)<n && p[j+1]=='*')
+    {
+        //if x*==""
+        if(solve(s,p,i,j+2))
+        return(true);
+
+        if(match)
+        return(solve(s,p,i+1,j));
+    }
+    
+    if(match)
+    return(solve(s,p,i+1,j+1));
+    else
+    return(false);
+}
     bool isMatch(string s, string p) {
-
-        dp.assign(
-            s.size() + 1,
-            vector<int>(p.size() + 1, -1)
-        );
-
-        return solve(s, p, 0, 0);
+        return(solve(s,p,0,0));
     }
 };
